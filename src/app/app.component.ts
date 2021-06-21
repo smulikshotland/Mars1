eaf3102ff402e1932d72d8ac18d1ed305c073c67
smulik shotlandgit config --global user.name smulik <shmolik1990@gmail.com>
2021-06-21 16:49:55 +0300

    ss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'mars';
-}
+
+export class AppComponent implements OnInit{
+
+   myTimer= setInterval(() => {
+      this.timer(); }, 1000);
+
+  ngOnInit(): void {  
+
+  }
+
+  count= localStorage.getItem("counterState")  ?localStorage.getItem("counterState") : "30";;
+  
+
+
+  timer(){
+    if (this.count!=null){
+      let countNumber = parseInt(this.count)
+      if(countNumber>0){
+        countNumber--
+        localStorage.setItem("counterState", countNumber.toString());
+        countNumber>=10?this.count=countNumber.toString():this.count=`0${countNumber.toString()}`
+      }
+      else{
+        alert("You missed the last rocket to mars!");
+        this.count="30"
+      }
+    }
+  
+    
+
+      
+    }
+    ResetTimer(){
+      
+     clearInterval(this.myTimer);
+      this.count="30"
+     this.myTimer= setInterval(() => { this.timer(); }, 1000);
+
+
+    }
+    
+    
+
+
+  }
+
+
+
